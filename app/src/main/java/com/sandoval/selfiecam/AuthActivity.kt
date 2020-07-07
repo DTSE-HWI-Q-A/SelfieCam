98a6dde9bc34707c3b7774b8e309f0ba41efe748
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2020-07-07 14:49:24 -0500

    003 Metodos de autenticacion de login con Huawei ID  - Metodo de login con huawei Id  - Icono de la aplicacion

--- app/src/main/java/com/sandoval/selfiecam/AuthActivity.kt
+++ app/src/main/java/com/sandoval/selfiecam/AuthActivity.kt
@@ -1,14 +1,55 @@
 package com.sandoval.selfiecam
 
-
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.huawei.hms.support.hwid.HuaweiIdAuthManager
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParams
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParamsHelper
+import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        btnLogin.setOnClickListener {
+            loginWithHuaweiID()
+        }
     }
 
+    private fun loginWithHuaweiID() {
+        val mAuthParams = HuaweiIdAuthParamsHelper(HuaweiIdAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+            .setEmail()
+            .setAccessToken()
+            .setProfile()
+            .setIdToken()
+            .setUid()
+            .setId()
+            .createParams()
+        val mAuthManager = HuaweiIdAuthManager.getService(this, mAuthParams)
+        startActivityForResult(mAuthManager.signInIntent, 1000)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1000) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(this, "Login Cancelado!", Toast.LENGTH_LONG).show()
+            } else if (resultCode == Activity.RESULT_OK) {
+                val authHuaweiTask = HuaweiIdAuthManager.parseAuthResultFromIntent(data)
+                if (authHuaweiTask.isSuccessful) {
+                    val huaweiAccount = authHuaweiTask.result
+                    Toast.makeText(
+                        this,
+                        "Login Existoso: ${huaweiAccount.displayName}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(this, "Fallo el login con Huawei ID", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
 }
